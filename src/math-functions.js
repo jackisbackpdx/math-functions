314f21861dac7907d0106755b480d7212fc12902
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-10-07 09:39:15 -0700

    The function multiplies an array with three numbmers

--- src/math-functions.js
+++ src/math-functions.js
@@ -101,7 +101,10 @@ you may continue to use the + operator for string concatenation.
 */
 
 export function multiplyArrayWithThreeNumbers(multArr) { //eslint-disable-line
-
+    let arr = [];
+    arr[0] = multiply(multiply(multArr[0], multArr[1])[0], multArr[2])[0];
+    arr[1] = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${arr[0]}.`;
+    return arr;
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
